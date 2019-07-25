60ae55cd802dd5640f40a4d59f7377d101f493b3
Ari Mendelow <ari@mendelow.me>
2019-07-25 14:42:12 -0500

    rails generate scaffold Resource uid:primary_key name:string skills:string portfolio:string start_date:date location:string manager:string

---
+++ db/migrate/20190725194153_create_resources.rb
@@ -0,0 +1,15 @@
+class CreateResources < ActiveRecord::Migration[5.2]
+  def change
+    create_table :resources do |t|
+      t.primary_key :uid
+      t.string :name
+      t.string :skills
+      t.string :portfolio
+      t.date :start_date
+      t.string :location
+      t.string :manager
+
+      t.timestamps
+    end
+  end
+end
