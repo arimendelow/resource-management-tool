2a2e0eb265beac1bca3bd1d5fce2ff22b17cd0b8
Ari Mendelow <ari@mendelow.me>
2019-07-25 14:22:21 -0500

    Change name of application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "sample-app_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "resource_management_tool_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
