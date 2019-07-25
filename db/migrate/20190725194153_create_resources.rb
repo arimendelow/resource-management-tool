9e9d9d34f1f090191c16691ee5011f65001d0824
Ari Mendelow <ari@mendelow.me>
2019-07-25 14:44:27 -0500

    Make 'skills' an array in the migration

--- db/migrate/20190725194153_create_resources.rb
+++ db/migrate/20190725194153_create_resources.rb
@@ -3,7 +3,7 @@ class CreateResources < ActiveRecord::Migration[5.2]
     create_table :resources do |t|
       t.primary_key :uid
       t.string :name
-      t.string :skills
+      t.string :skills, array: true, default: []
       t.string :portfolio
       t.date :start_date
       t.string :location
