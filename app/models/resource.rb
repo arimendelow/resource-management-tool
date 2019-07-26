a58e66aab562d5bc1a9c092db9b2f6e1a6b69a1a
Ari Mendelow <ari@mendelow.me>
2019-07-26 15:07:09 -0500

    Add some resource requirements

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,2 +1,5 @@
 class Resource < ApplicationRecord
+  ONLY_NUMBERS_REGEX = /\A[0-9]+\z/
+  validates :uid, presence: true, format: { with: ONLY_NUMBERS_REGEX }
+  validates :name, presence: true
 end
