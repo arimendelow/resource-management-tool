ff642ae79ff174c9732cb65dcff35f2ece8ca58d
Ari Mendelow <ari@mendelow.me>
2019-07-26 14:28:06 -0500

    Now ALL resources actions are behind the log-in wall, for real

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,6 +1,6 @@
 class ResourcesController < ApplicationController
   before_action :set_resource, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user,  only: [:index, :show, :new, :edit, :create, :update, :destroy]
+  before_action :logged_in_user
 
   # GET /resources
   # GET /resources.json
