28a2a401c25ae09f7c0a72f0734b017fba213ff5
Ari Mendelow <ari@mendelow.me>
2019-07-31 15:03:41 -0500

    Add a comment

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,5 @@
 class Resource < ApplicationRecord
+  # A constant, indicated in Ruby by a name starting with a capital letter
   ONLY_NUMBERS_REGEX = /\A[0-9]+\z/
   validates :uid, presence: true, format: { with: ONLY_NUMBERS_REGEX }, uniqueness: true
   validates :name, presence: true
