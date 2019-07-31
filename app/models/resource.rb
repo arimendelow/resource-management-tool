de72bff07cd5138f022ed8f4179dbd9f1a0a5a2a
Ari Mendelow <ari@mendelow.me>
2019-07-31 14:35:46 -0500

    Add uniqueness validation here to catch the error before Rails attempts to make a call to the database (which will cause a worse error)

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,5 +1,5 @@
 class Resource < ApplicationRecord
   ONLY_NUMBERS_REGEX = /\A[0-9]+\z/
-  validates :uid, presence: true, format: { with: ONLY_NUMBERS_REGEX }
+  validates :uid, presence: true, format: { with: ONLY_NUMBERS_REGEX }, uniqueness: true
   validates :name, presence: true
 end
