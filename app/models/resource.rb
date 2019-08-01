932538c5a6fef2305c36fe727af2e236ccf1e1f5
Ari Mendelow <ari@mendelow.me>
2019-08-01 15:00:12 -0500

    nope nevermind that didn't work... neither does this though

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,6 @@
 class Resource < ApplicationRecord
   # A constant, indicated in Ruby by a name starting with a capital letter
-  ONLY_NUMBERS_REGEX = /\Ad+\z/
+  ONLY_NUMBERS_REGEX = /\A\d+\z/
   validates :uid, presence: true, format: { with: ONLY_NUMBERS_REGEX }, uniqueness: true
   validates :name, presence: true
 end
